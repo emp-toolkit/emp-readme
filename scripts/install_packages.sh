1940ac48c34fe767d2c983962e71c2cd284bb561
WANG,Xiao <wangxiao1254@gmail.com>
2017-04-18 00:44:43 -0400

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,4 +1,4 @@
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:george-edison55/cmake-3.x
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo apt-get update
-sudo apt-get install cmake git build-essential libssl-dev libgmp-dev
+sudo apt-get install -y cmake git build-essential libssl-dev libgmp-dev
