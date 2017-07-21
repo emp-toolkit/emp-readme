38e17acb805933e08bfb1828b09bb7c5946ab759
WANG,Xiao <wangxiao1254@gmail.com>
2017-07-21 03:03:07 -0400

    Update setup_new_machine.sh

--- scripts/setup_new_machine.sh
+++ scripts/setup_new_machine.sh
@@ -1,8 +1,7 @@
 git clone https://github.com/emp-toolkit/emp-readme.git
-DIR= ./emp-readme/scripts
-bash $DIR/install_packages.sh
-bash $DIR/install_relic.sh
-bash $DIR/install_emp-tool.sh
-bash $DIR/install_emp-ot.sh
-bash $DIR/install_emp-m2pc.sh
-bash $DIR/install_emp-sh2pc.sh
+bash ./emp-readme/scripts/install_packages.sh
+bash ./emp-readme/scripts/install_relic.sh
+bash ./emp-readme/scripts/install_emp-tool.sh
+bash ./emp-readme/scripts/install_emp-ot.sh
+bash ./emp-readme/scripts/install_emp-m2pc.sh
+bash ./emp-readme/scripts/install_emp-sh2pc.sh
