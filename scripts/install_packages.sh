fc30873711b0e03d8e0491a02fefcc704b131c97
WANG,Xiao <wangxiao1254@gmail.com>
2017-08-17 11:03:52 -0400

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,4 +1,4 @@
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo apt-get update
-sudo apt-get install -y cmake git build-essential libssl-dev libgmp-dev python
+sudo apt-get install -y cmake git build-essential libssl-dev libgmp-dev python libboost-all-dev
