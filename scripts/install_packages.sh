2d89ecddbd21b36826f35b160516f65dd8752c3e
Weikeng Chen <w.k@berkeley.edu>
2017-10-28 19:59:40 -0700

    Add brew list xxx ||

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,10 +1,10 @@
 if [ "$(uname)" == "Darwin" ]; then
 	brew update
-	brew install openssl
-	brew install xctool
-	brew install cmake
-	brew install gmp
-	brew install boost
+	brew list openssl || brew install openssl
+	brew list xctool || brew install xctool
+	brew list cmake || brew install cmake
+	brew list gmp || brew install gmp
+	brew list boost || brew install boost
 else
 	sudo apt-get install -y software-properties-common
 	sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
