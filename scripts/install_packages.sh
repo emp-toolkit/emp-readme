0b4fd7756691730bfeeac5850a4ad0f70d868740
Ubuntu <ubuntu@ip-172-31-65-98.ec2.internal>
2017-12-26 18:48:27 +0000

    install boost script

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -9,11 +9,9 @@ if [ "$(uname)" == "Darwin" ]; then
 else
 	sudo apt-get install -y software-properties-common
 	sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
-	#sudo add-apt-repository -y ppa:kzemek/boost
+	sudo add-apt-repository -y ppa:kojoley/boost
 	sudo apt-get update
-	sudo apt-get install -y cmake git build-essential libssl-dev libgmp-dev python
-	#boost
-	wget http://mirrors.kernel.org/ubuntu/pool/universe/b/boost-defaults/libboost-all-dev_1.58.0.1ubuntu1_amd64.deb
-	sudo dpkg -i libboost-all-dev_1.58.0.1ubuntu1_amd64.deb
-	sudo apt-get install -f
+	sudo apt-get install -y cmake git build-essential libssl-dev libgmp-dev python 
+	sudo apt-get install libboost1.58-dev
+	sudo apt-get install sudo apt-get install libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}1.58-dev
 fi
