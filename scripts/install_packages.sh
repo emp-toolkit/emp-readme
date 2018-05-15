0cd8f091d53e7988dc40517e3bbcc136374390cd
Xiao Wang <wangxiao1254@gmail.com>
2018-05-14 21:33:00 -0400

    install check vesion number

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -10,6 +10,14 @@ else
 	sudo apt-get install -y software-properties-common
 	sudo apt-get update
 	sudo apt-get install -y cmake git build-essential libssl-dev libgmp-dev python 
-	sudo apt-get install -y libboost-dev
-	sudo apt-get install -y libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}-dev
+
+	CC=`lsb_release -rs | cut -c 1-2`
+	VER= expr $CC + 0
+	if [[ $VER -gt 13 ]]; then
+		sudo apt-get install -y libboost-dev
+		sudo apt-get install -y libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}-dev
+	else
+		sudo apt-get install -y libboost1.58-dev
+		sudo apt-get install -y libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}1.58-dev
+	fi
 fi
