85f8d5295b63e7c7e31fa5b0bb83989b1880ce24
Xiao Wang <wangxiao1254@gmail.com>
2018-05-14 21:37:08 -0400

    fix install

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -7,16 +7,20 @@ if [ "$(uname)" == "Darwin" ]; then
 	brew list gmp || brew install gmp
 	brew list boost || brew install boost
 else
-	sudo apt-get install -y software-properties-common
-	sudo apt-get update
-	sudo apt-get install -y cmake git build-essential libssl-dev libgmp-dev python 
-
 	CC=`lsb_release -rs | cut -c 1-2`
 	VER= expr $CC + 0
 	if [[ $VER -gt 13 ]]; then
+		sudo apt-get install -y software-properties-common
+		sudo apt-get update
+		sudo apt-get install -y cmake git build-essential libssl-dev libgmp-dev python 
 		sudo apt-get install -y libboost-dev
 		sudo apt-get install -y libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}-dev
 	else
+		sudo apt-get install -y software-properties-common
+		sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
+		sudo add-apt-repository -y ppa:kojoley/boost
+		sudo apt-get update
+		sudo apt-get install -y cmake git build-essential libssl-dev libgmp-dev python 
 		sudo apt-get install -y libboost1.58-dev
 		sudo apt-get install -y libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}1.58-dev
 	fi
