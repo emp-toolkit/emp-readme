106593a65740d6972222dbb3921f1edc25c49856
Xiao Wang <wangxiao1254@gmail.com>
2019-11-13 14:20:53 -0600

    Update setup_new_machine.sh

--- scripts/setup_new_machine.sh
+++ scripts/setup_new_machine.sh
@@ -1,6 +1,5 @@
 git clone https://github.com/emp-toolkit/emp-readme.git
 bash ./emp-readme/scripts/install_packages.sh
-bash ./emp-readme/scripts/install_relic.sh
 bash ./emp-readme/scripts/install_emp-tool.sh
 bash ./emp-readme/scripts/install_emp-ot.sh
 bash ./emp-readme/scripts/install_emp-m2pc.sh
