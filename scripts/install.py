5e65e7d87ea38a268d98e0e8f318a05e0e9f3665
Xiao Wang <wangxiao1254@gmail.com>
2022-10-14 10:24:58 -0500

    Update install.py

--- scripts/install.py
+++ scripts/install.py
@@ -3,10 +3,10 @@ import subprocess
 install_packages = '''
 if [ "$(uname)" == "Darwin" ]; then
 	brew update
-	brew list openssl || brew install openssl
-	#brew list xctool || brew install xctool
-	brew list pkg-config || brew install pkg-config
-	brew list cmake || brew install cmake
+	brew install openssl
+	brew install xctool
+	brew install pkg-config
+	brew install cmake
 else
     if command -v apt-get >/dev/null; then
         sudo apt-get install -y software-properties-common
